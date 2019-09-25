4007b05baa26102d71479c5953ea57a099326873
Albert wang <awang21@ssis.edu.vn>
2019-09-25 18:57:42 +0700

    Update isbn.py

--- isbn.py
+++ isbn.py
@@ -1,15 +1,28 @@
+#Albert
+'''
+The code shown below validates ISBN-10 numbers. If the user inputs an ISBN
+number that doesn't have exactly 10 digits or if its made up of none-digits, it
+will reprompt user.
+'''
+
 isbn_number = input("ISBN: ")
-new_number = 0
+
+while len(isbn_number) != 10 or isbn_number.isdigit() == False:
+    isbn_number = input("Retry: ")
+
+sum_of_digits = 0
 n = 1
 
-while len(isbn_number) != 10:
-    isbn_number = input("Retry: ")
-    
+'''
+This loop adds the ISBN digits mulltiplied by its corresponding order number
+together
+'''
+
 for i in isbn_number:
-    new_number += int(i) * n
+    sum_of_digits += int(i) * n
     n += 1
-    
-if new_number % 11 == 0:
+
+if sum_of_digits % 11 == 0:
     print("YES")
     
 else:
